59a433f2a56a6aa84fea6a219a9a583a6875e7d8
ValentinaKry <98461511+ValentinaKry@users.noreply.github.com>
2022-07-19 12:41:44 +0300

    Add files via upload

---
+++ Homework1/Homework1/ContentView.swift
@@ -0,0 +1,124 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedIndex: Int = 0
+    private let locations = ["All", "Beach", "Sea"]
+    var body: some View {
+        VStack {
+
+            AppBarView()
+            TagLineView()
+
+            SearchView()
+
+            Spacer()
+            ScrollView (.horizontal, showsIndicators: false){
+                HStack {
+                    ForEach(0..<locations.count, id: \.self) { i in
+                        CategoryView(isActive: i == selectedIndex, text: locations[i])
+                            .onTapGesture {
+                                selectedIndex = i
+                            }
+                    }
+                }
+            }
+            .padding(.horizontal)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach (0..<2) { index in
+                        PhotoGallery(image: Image("view_\(index+1)"))
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct AppBarView: View {
+    var body: some View {
+        HStack {
+            Button (action: {}){
+                Image(systemName: "location.fill")
+                    .foregroundColor(Color("secondary"))
+                    .font(.title2)
+            }
+
+            Text("Los Angeles, CA")
+                .foregroundColor(Color("secondary"))
+                .font(.title3)
+            Spacer()
+
+            Button(action: {}) {
+                Image("Avatar")
+                    .resizable()
+                    .frame(width: 55, height: 45)
+                    .clipShape(Circle())
+                    .frame(width: 70, height: 70)
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct TagLineView: View {
+    var body: some View {
+        HStack() {
+            Text("Discover New Destination")
+                .padding(.leading)
+                .font(.system(size: 35, weight: .bold))
+            Spacer()
+        }
+    }
+}
+
+struct SearchView: View {
+    @State private var search: String = ""
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(Color.gray)
+                    .padding(.trailing, 8)
+                TextField("Search Places", text: $search)
+            }
+            .padding()
+            .background(Color("lightGray"))
+            .clipShape(Capsule())
+
+            Button (action: {}) {
+                Image(systemName: "slider.horizontal.3")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color("secondary"))
+                    .clipShape(Circle())
+                    .padding(.trailing)
+            }
+        }
+        .padding(.top, 25)
+        .padding(.leading)
+    }
+}
+
+struct CategoryView: View {
+    let isActive : Bool
+    let text: String
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(text)
+                .font(.system(size: 16, weight: .bold))
+                .foregroundColor(isActive ? Color("secondary") : Color.gray)
+
+
+        }
+        .padding(.trailing)
+    }
+}
---
+++ Homework1/Homework1/Homework1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework1App.swift
+//  Homework1
+//
+//  Created by Valentina Krysak on 19.07.22.
+//
+
+import SwiftUI
+
+@main
+struct Homework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
