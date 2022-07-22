6508968a23b8542aa9f95ecfebf590320c552604
ValentinaKry <98461511+ValentinaKry@users.noreply.github.com>
2022-07-22 15:39:33 +0300

    Add files via upload

---
+++ Homework1.1/Homework1.1/Homework1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework1_1App.swift
+//  Homework1.1
+//
+//  Created by Valentina Krysak on 21.07.22.
+//
+
+import SwiftUI
+
+@main
+struct Homework1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
