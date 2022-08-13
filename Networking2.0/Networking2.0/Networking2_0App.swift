b1b3596b70e56791a40a1dcafb37eeab8b9aa1d3
ValentinaKry <98461511+ValentinaKry@users.noreply.github.com>
2022-08-13 19:16:52 +0300

    Add files via upload

---
+++ Networking2.0/Networking2.0/Networking2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Networking2_0App.swift
+//  Networking2.0
+//
+//  Created by Valentina Krysak on 13.08.22.
+//
+
+import SwiftUI
+
+@main
+struct Networking2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
